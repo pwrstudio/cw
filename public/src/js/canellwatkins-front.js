8ae4a97d877fe924d222a0bc3e086168f3be3c04
root <root@ip-172-31-47-20.ec2.internal>
2015-06-29 13:27:41 +0000

    iamge updates

--- public/src/js/canellwatkins-front.js
+++ public/src/js/canellwatkins-front.js
@@ -1,45 +1,48 @@
+$.fn.preload = function () {
+    $('<img/>')[0].src = this;
+}
+
 $(document).ready(function () {
 
-	$.fn.slideFadeToggle = function (speed, easing, callback) {
-		return this.animate({
-			opacity: 'toggle',
-			height: 'toggle'
-		}, speed, easing, callback);
-	};
+  $.fn.slideFadeToggle = function (speed, easing, callback) {
+    return this.animate({
+      opacity: 'toggle',
+      height: 'toggle'
+    }, speed, easing, callback);
+  };
 
-	var socket = io();
+  var socket = io();
 
-	socket.on('traced', function (msg) {
-		if (msg != null) {
-			var source = $("#trace-template").html();
-			var template = Handlebars.compile(source);
-			$("#trace-container").append(template(msg));
-		}
-	});
+  socket.on('traced', function (msg) {
+    if (msg != null) {
+      var source = $("#trace-template").html();
+      var template = Handlebars.compile(source);
+      $("#trace-container").append(template(msg));
+    }
+  });
 
-	$(document).on("click", ".image-link", function (e) {
-		e.preventDefault();
-		//		$(this).parent().html("<a href='" + $(this).data("large") + "' class='image-thumb'><img class='image-thumb' src='" + $(this).attr("href") + "' alt='" + $(this).data("title") + "'></a>");
-		$(this).next(".thumb-container").slideFadeToggle();
-	});
+  $(document).on("click", ".image-link", function (e) {
+    e.preventDefault();
+    var container = $(this).next(".thumb-container");
+    container.slideFadeToggle();
+    var large = container.children("img").data("large");
+    console.log(large)
+    $('<img/>')[0].src = large;
+  });
 
-	//	$(document).on("click", ".image-thumb", function (e) {
-	//		//		e.preventDefault();
-	//		//		$(this).parent().html("<img class='image-thumb' src='" + $(this).attr("href") + "'>");
-	//
-	//	});
+  $('.lightBox').on('click', function (e) {
+    $("body").addClass("no-scroll");
+    var largeLink = '<img src="' + $(this).data("large") + '">';
+    $("#overlay").html(largeLink);
+    $("#overlay").fadeIn();
+    e.preventDefault();
+  });
 
-	$('.lightBox').on('click', function (e) {
-		var largeLink = '<img src="' + $(this).attr("href") + '">';
-		$("#overlay").html(largeLink);
-		$("#overlay").fadeIn();
-		e.preventDefault();
-	});
-
-	$('#overlay').on('click', function (e) {
-		$("#overlay").fadeOut();
-		e.preventDefault();
-	});
+  $('#overlay').on('click', function (e) {
+    $("body").removeClass("no-scroll");
+    $("#overlay").fadeOut();
+    e.preventDefault();
+  });
 
 
 });
