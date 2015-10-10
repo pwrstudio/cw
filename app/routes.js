8dab238c654645456215a82e31d7ec2b5acc7399
cell@pwr.site <cell@pwr.site>
2015-10-10 03:36:36 +0200

    Clean up Strato

--- app/routes.js
+++ app/routes.js
@@ -6,140 +6,140 @@
 
 
   var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+      LocalStrategy = require('passport-local').Strategy;
 
   var auth = require('./handlers/auth.js'),
-    content = require('./handlers/content.js'),
-    collection = require('./handlers/collection.js'),
-    exhibition = require('./handlers/exhibition.js'),
-    publication = require('./handlers/publication.js'),
-    frontend = require('./handlers/frontend.js'),
-    container = require('./handlers/container.js'),
-    pdf = require('./handlers/pdf.js'),
-    meta = require('./handlers/meta.js');
+      content = require('./handlers/content.js'),
+      collection = require('./handlers/collection.js'),
+      exhibition = require('./handlers/exhibition.js'),
+      publication = require('./handlers/publication.js'),
+      frontend = require('./handlers/frontend.js'),
+      container = require('./handlers/container.js'),
+      pdf = require('./handlers/pdf.js'),
+      meta = require('./handlers/meta.js');
 
 
   module.exports = function (app, io) {
 
 
-    console.log(pdf);
+      console.log(pdf);
 
-    /*
-     *
-     *  Authentication
-     *
-     */
+      /*
+       *
+       *  Authentication
+       *
+       */
 
-    app.get('/signup', auth.signup_form);
+      app.get('/signup', auth.signup_form);
 
-    app.get('/login', auth.login_form);
+      app.get('/login', auth.login_form);
 
-    app.get('/logout', auth.isLoggedIn, auth.logout);
+      app.get('/logout', auth.isLoggedIn, auth.logout);
 
-    app.post('/login', passport.authenticate('local-login', {
-      successRedirect: '/infra',
-      failureRedirect: '/login'
-    }));
+      app.post('/login', passport.authenticate('local-login', {
+          successRedirect: '/infra',
+          failureRedirect: '/login'
+      }));
 
-    app.post('/signup', passport.authenticate('local-signup', {
-      successRedirect: '/',
-      failureRedirect: '/signup'
-    }));
+      app.post('/signup', passport.authenticate('local-signup', {
+          successRedirect: '/',
+          failureRedirect: '/signup'
+      }));
 
-    app.get('/check_session', function (req, res) {
-      if (req.isAuthenticated()) {
-        res.send(req.user.email);
-      } else {
-        res.send("false");
-      }
-    });
-
-
-    /*
-     *
-     *  API: Content
-     *
-     */
-
-    app.get('/api/content/get/:count', content.get_content);
-    app.get('/api/content/single/:id', content.get_content_by_id);
-    app.post('/api/content/post/image', content.post_image_content);
-    app.post('/api/content/update/image/:id', content.update_image_content);
-    app.post('/api/content/update/text/:id', content.update_text_content);
-    app.post('/api/content/post/text', content.post_text_content);
-    app.delete('/api/content/del/image/:id', content.delete_image_content);
-    app.delete('/api/content/del/text/:id', content.delete_text_content);
-
-    /*
-     *
-     *  API: Exhibition
-     *
-     */
-
-    app.get('/api/exhibition', exhibition.get_exhibition);
-    app.post('/api/exhibition', exhibition.post_exhibition);
-    app.post('/api/update/exhibition/:id', exhibition.update_exhibition);
-
-    /*
-     *
-     *  API: Publication
-     *
-     */
-
-    app.get('/api/publication', publication.get_publication);
-    app.post('/api/publication', publication.post_publication);
-    app.post('/api/update/publication/:id', publication.update_publication);
-
-    /*
-     *
-     *  API: Container (Exhibition + publication)
-     *
-     */
-
-    app.delete('/api/container/:id', container.delete);
-
-    /*
-     *
-     *  API: Collection
-     *
-     */
-
-    app.delete('/api/collection/del/:id', collection.delete_collection);
-    app.get('/api/collection/', collection.get_collection);
-    app.get('/api/collection/single/:id', collection.get_collection_by_id);
-    app.post('/api/collection/', collection.make_collection);
+      app.get('/check_session', function (req, res) {
+          if (req.isAuthenticated()) {
+              res.send(req.user.email);
+          } else {
+              res.send("false");
+          }
+      });
 
 
-    /*
-     *
-     *  PDF
-     *
-     */
-    app.get('/pdf/:slug', pdf.generatePdf);
+      /*
+       *
+       *  API: Content
+       *
+       */
+
+      app.get('/api/content/get/:count', content.get_content);
+      app.get('/api/content/single/:id', content.get_content_by_id);
+      app.post('/api/content/post/image', content.post_image_content);
+      app.post('/api/content/update/image/:id', content.update_image_content);
+      app.post('/api/content/update/text/:id', content.update_text_content);
+      app.post('/api/content/post/text', content.post_text_content);
+      app.delete('/api/content/del/image/:id', content.delete_image_content);
+      app.delete('/api/content/del/text/:id', content.delete_text_content);
+
+      /*
+       *
+       *  API: Exhibition
+       *
+       */
+
+      app.get('/api/exhibition', exhibition.get_exhibition);
+      app.post('/api/exhibition', exhibition.post_exhibition);
+      app.post('/api/update/exhibition/:id', exhibition.update_exhibition);
+
+      /*
+       *
+       *  API: Publication
+       *
+       */
+
+      app.get('/api/publication', publication.get_publication);
+      app.post('/api/publication', publication.post_publication);
+      app.post('/api/update/publication/:id', publication.update_publication);
+
+      /*
+       *
+       *  API: Container (Exhibition + publication)
+       *
+       */
+
+      app.delete('/api/container/:id', container.delete);
+
+      /*
+       *
+       *  API: Collection
+       *
+       */
+
+      app.delete('/api/collection/del/:id', collection.delete_collection);
+      app.get('/api/collection/', collection.get_collection);
+      app.get('/api/collection/single/:id', collection.get_collection_by_id);
+      app.post('/api/collection/', collection.make_collection);
 
 
-    /*
-     *
-     *  Metadata
-     *
-     */
-    app.get('/api/meta', meta.get_meta);
-    app.post('/api/meta', meta.set_meta);
+      /*
+       *
+       *  PDF
+       *
+       */
+      app.get('/pdf/:slug', pdf.generatePdf);
 
-    /*
-     *
-     *  FRONTEND
-     *
-     */
 
-    app.get('/infra', auth.isLoggedIn, frontend.infra);
-    app.get('/', frontend.index);
+      /*
+       *
+       *  Metadata
+       *
+       */
+      app.get('/api/meta', meta.get_meta);
+      app.post('/api/meta', meta.set_meta);
 
-    // TEMP
-    app.get('/intro1', frontend.intro1);
-    app.get('/intro2', frontend.intro2);
+      /*
+       *
+       *  FRONTEND
+       *
+       */
 
-    app.get('/collection/:slug', frontend.collection);
-    app.get('*', frontend.fallback);
+      app.get('/infra', auth.isLoggedIn, frontend.infra);
+      app.get('/', frontend.index);
+
+      // TEMP
+      //    app.get('/intro1', frontend.intro1);
+      //    app.get('/intro2', frontend.intro2);
+
+      app.get('/collection/:slug', frontend.collection);
+      app.get('*', frontend.fallback);
 
   };
--- public/src/js/canellwatkins-front.js
+++ public/src/js/canellwatkins-front.js
@@ -22,56 +22,23 @@ var contextClass = (window.AudioContext ||
 if (contextClass) {
   // Web Audio API is available.
   var context = new contextClass();
-  var gainValue = 0.1;
   var gainNode;
   var oscillator;
-
-  console.log(context);
-
-} else {
-  alert("error");
-}
-
-
-/*
- *
- *  Frequency sweep
- *
- */
-
-function sweep(freq1, freq2, duration) {
-
-  currTime = context.currentTime;
 
   oscillator = context.createOscillator();
   gainNode = context.createGain ? context.createGain() : context.createGainNode();
 
-  oscillator.frequency.value = freq1;
+  oscillator.frequency.value = 3000;
+  gainNode.gain.value = 0.05;
 
   oscillator.connect(gainNode);
   gainNode.connect(context.destination);
 
   oscillator.type = "sine";
-  oscillator.start ? oscillator.start(0) : oscillator.noteOn(0);
 
-  vol1 = 0.5;
-  vol2 = 0;
-
-  vol1 = Math.min(vol1, 0.5);
-  vol2 = Math.min(vol2, 0.5);
-
-  gainNode.gain.value = vol1;
-
-  oscillator.frequency.linearRampToValueAtTime(freq1, currTime);
-  gainNode.gain.linearRampToValueAtTime(vol1, currTime);
-  oscillator.frequency.linearRampToValueAtTime(freq2, currTime + duration * 1);
-  gainNode.gain.linearRampToValueAtTime(vol2, currTime + duration * 1);
-
-
-  oscillator.stop ? oscillator.stop(currTime + duration * 1) : oscillator.noteOff(currTime + duration * 1);
-
-};
-
+} else {
+  alert("error");
+}
 
 /*
  *
@@ -98,28 +65,6 @@ function compare(a, b) {
   return 0;
 }
 
-
-/*
- *
- *  Count-up to distance
- *
- */
-
-function startTimer(distance) {
-  console.log(distance);
-  var timeInSeconds = distance / 2000;
-  console.log(timeInSeconds);
-  sweep(1, 15000, timeInSeconds);
-  $("#startUpOverlay").css("transition-duration", timeInSeconds + "s");
-  $("#startUpOverlay").css("-webkit-transition-duration", timeInSeconds + "s");
-  var counter = setTimeout(function () {
-    clearInterval(counter);
-    renderTraceQ();
-    $("#counterOverlay").hide();
-    $(".content-columns").show();
-  }, (timeInSeconds * 1000));
-}
-
 /*
  *
  *  Render traceroute
@@ -127,13 +72,11 @@ function startTimer(distance) {
  */
 
 function renderTraceQ() {
-
   traceResults.sort(compare);
-
   traceResults.forEach(function (arrayItem) {
     $("#trace-container").append(MyApp.templates.trace(arrayItem));
   });
-
+  $("a div").lettering();
 }
 
 
@@ -149,12 +92,8 @@ function renderTraceQ() {
 
 $(document).ready(function () {
 
-
-  $(".loadtime").text("Document loaded in " + (Date.now() - timerStart) + "ms");
-
-
-  $("#startUpOverlay").addClass("there");
-
+  oscillator[oscillator.start ? 'start' : 'noteOn'](0);
+  
   /*
    *
    *  Sockets client
@@ -163,27 +102,38 @@ $(document).ready(function () {
 
   var socket = io();
 
+  socket.on('tracedone', function (msg) {
+    setTimeout(function () {
+      $("#tracestart-container").html(MyApp.templates.tracestart(msg));
+      setTimeout(function () {
+        oscillator.stop(0);
+        renderTraceQ();
+        $("#counterOverlay").hide();
+        $(".content-columns").show();
+        $("#cable-length").html(msg.total);
+      }, 1500);
+    }, 700);
+
+  });
+
   socket.on('traced', function (msg) {
+    console.log("traced");
     if (msg !== null) {
       traceResults.push(msg);
       $("#tracestart-container").html(MyApp.templates.tracestart(msg));
     }
   });
 
-  socket.on('tracedone', function (msg) {
-    setTimeout(function () {
-      $("#tracestart-container").html("Roundtrip time: " + msg.roundtrip + "ms")
-      $("#roundtrip").html(msg.roundtrip + "ms")
-    }, 500);
-  });
-
-  console.log($("#distance").data("distance"));
-  startTimer($("#distance").data("distance"));
+  /*
+   *
+   * Interaction
+   *
+   */
 
   $(document).on("click", ".image-link", function (e) {
     e.preventDefault();
     var container = $(this).next(".thumb-container");
-    container.slideToggle(1000);
+    container.slideToggle(700);
     var large = container.children("img").data("large");
     $('<img/>')[0].src = large;
   });
